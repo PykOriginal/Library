507f2fb646d92beecfba8c4f757a4e9531ccfea4
PykOriginal <131517265+PykOriginal@users.noreply.github.com>
2023-04-23 14:21:28 +0300

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 9848789324 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Ragdoll%20Engine"))()
+end
+if game.PlaceId == 6205205961 or 6364879587 or 6364881161 or 6391540653 or 6391642302 or 6463986562 or 6481131111 or 6507405695 or 6815760584 or 8709253565 or 6413499953 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Escape%20Running%20Head.lua"))()
+end
