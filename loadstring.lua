12dc4d05b83d8fe48b0ceb65abdb5a9cf307676b
PykOriginal <131517265+PykOriginal@users.noreply.github.com>
2023-04-25 16:31:07 +0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -4,3 +4,9 @@ end
 if game.PlaceId == 6205205961 or 6364879587 or 6364881161 or 6391540653 or 6391642302 or 6463986562 or 6481131111 or 6507405695 or 6815760584 or 8709253565 or 6413499953 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Escape%20Running%20Head.lua"))()
 end
+if game.PlaceId == 8888615802 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Rainbow%20Friends.lua"))()
+end
+if game.PlaceId == 7991339063 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Rainbow%20Friends%20no%20game.lua"))()
+end
