ba297f00630029a8c6103bd092031b1744c11ff3
PykOriginal <131517265+PykOriginal@users.noreply.github.com>
2023-04-25 16:50:51 +0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 9848789324 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Ragdoll%20Engine"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Ragdoll%20Engine.lua"))()
 end
-if game.PlaceId == 6205205961 or 6364879587 or 6364881161 or 6391540653 or 6391642302 or 6463986562 or 6481131111 or 6507405695 or 6815760584 or 8709253565 or 6413499953 then
+if game.PlaceId == 6205205961 or game.PlaceId == 6364879587 or game.PlaceId == 6364881161 or game.PlaceId == 6391540653 or game.PlaceId == 6391642302 or game.PlaceId == 6463986562 or game.PlaceId == 6481131111 or game.PlaceId == 6507405695 or game.PlaceId == 6815760584 or game.PlaceId == 8709253565 or game.PlaceId == 6413499953 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Escape%20Running%20Head.lua"))()
 end
 if game.PlaceId == 8888615802 then
