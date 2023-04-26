482deb747522394b83849f43482e684cc491be5d
PykOriginal <131517265+PykOriginal@users.noreply.github.com>
2023-04-26 14:28:09 +0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -11,6 +11,6 @@ end
 if game.PlaceId == 7991339063 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Rainbow%20Friends%20no%20game.lua"))()
 end
-if Game ~= 7991339063 or Game == 8888615802 or 6205205961 or game.PlaceId == 6364879587 or game.PlaceId == 6364881161 or game.PlaceId == 6391540653 or game.PlaceId == 6391642302 or game.PlaceId == 6463986562 or game.PlaceId == 6481131111 or game.PlaceId == 6507405695 or game.PlaceId == 6815760584 or game.PlaceId == 8709253565 or game.PlaceId == 6413499953 or Game == 9848789324 then
+if Game ~= 7991339063 or Game ~= 8888615802 or Game ~= 6205205961 or game.PlaceId ~= 6364879587 or game.PlaceId ~= 6364881161 or game.PlaceId ~= 6391540653 or game.PlaceId ~=6391642302 or game.PlaceId ~=6463986562 or game.PlaceId ~=6481131111 or game.PlaceId ~=6507405695 or game.PlaceId ~=5760584 or game.PlaceId ~=709253565 or game.PlaceId ~= 6413499953 or Game ~= 9848789324 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Pyk%20Hub%20Not%20Made%20Script.lua"))()
 end
