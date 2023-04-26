d0a7ee8ac4901a18dbee780a6725ed95fc0bd234
PykOriginal <131517265+PykOriginal@users.noreply.github.com>
2023-04-26 18:04:54 +0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,16 +1,16 @@
 local Game = game.PlaceId
 if game.PlaceId == 9848789324 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Ragdoll%20Engine"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Ragdoll%20Engine"))()
 end
 if game.PlaceId == 6205205961 or game.PlaceId == 6364879587 or game.PlaceId == 6364881161 or game.PlaceId == 6391540653 or game.PlaceId == 6391642302 or game.PlaceId == 6463986562 or game.PlaceId == 6481131111 or game.PlaceId == 6507405695 or game.PlaceId == 6815760584 or game.PlaceId == 8709253565 or game.PlaceId == 6413499953 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Escape%20Running%20Head.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Escape%20Running%20Head.lua"))()
 end
 if game.PlaceId == 8888615802 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Rainbow%20Friends.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Rainbow%20Friends.lua"))()
 end
 if game.PlaceId == 7991339063 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Rainbow%20Friends%20no%20game.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Rainbow%20Friends%20no%20game.lua"))()
 end
-if Game ~= 7991339063 or Game ~= 8888615802 or Game ~= 6205205961 or game.PlaceId ~= 6364879587 or game.PlaceId ~= 6364881161 or game.PlaceId ~= 6391540653 or game.PlaceId ~=6391642302 or game.PlaceId ~=6463986562 or game.PlaceId ~=6481131111 or game.PlaceId ~=6507405695 or game.PlaceId ~=5760584 or game.PlaceId ~=709253565 or game.PlaceId ~= 6413499953 or Game ~= 9848789324 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Pyk%20Hub%20Not%20Made%20Script.lua"))()
+if Game ~= 7991339063 and Game ~= 8888615802 and Game ~= 6205205961 and game.PlaceId ~= 6364879587 and game.PlaceId ~= 6364881161 and game.PlaceId ~= 6391540653 and game.PlaceId ~= 6391642302 and game.PlaceId ~= 6463986562 and game.PlaceId ~= 6481131111 and game.PlaceId ~= 6507405695 and game.PlaceId ~= 6815760584 and game.PlaceId ~= 8709253565 and game.PlaceId ~= 6413499953 and Game ~= 9848789324 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/PykOriginal/Pyk-Hub/main/Pyk%20Hub%20Not%20Made%20Script.lua"))()
 end
